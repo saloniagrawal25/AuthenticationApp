c859c180e2883a8b4096bdb18614b018a2270279
Saloni Agrawal <saloniagrawal250797@gmail.com>
2020-02-02 17:27:22 +0530

    Updated UI for Login Page

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,36 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <LoginPage />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import LoginPage from "./components/LoginPage.vue";
 
 export default {
-  name: 'app',
+  name: "app",
   components: {
-    HelloWorld
+    LoginPage
   }
-}
+};
 </script>
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  font-family: Tahoma;
+  font-size: 1rem;
+  color: #222;
+  background-color: #092525;
+  width: 100%;
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-image: url("./assets/Connected-Homes-Main-Image-2.jpg");
+  background-size: cover;
+}
+body {
+  margin: 0;
+  padding: 0;
 }
 </style>
