d422962deccc428303f1f2f1b68dc2e8fd4dd1df
Saloni Agrawal <saloniagrawal250797@gmail.com>
2020-02-02 19:04:46 +0530

    Page Optimized

--- src/App.vue
+++ src/App.vue
@@ -16,18 +16,20 @@ export default {
 </script>
 
 <style>
+@import url("https://fonts.googleapis.com/css?family=Montserrat:400,700&display=swap");
 #app {
-  font-family: Tahoma;
+  font-family: "Montserrat", sans-serif;
   font-size: 1rem;
   color: #222;
-  background-color: #092525;
+  background-color: #004593;
   width: 100%;
   height: 100vh;
   display: flex;
   justify-content: center;
   align-items: center;
-  background-image: url("./assets/Connected-Homes-Main-Image-2.jpg");
+  background-image: url("./assets/Background.jpg");
   background-size: cover;
+  margin: 0;
 }
 body {
   margin: 0;
