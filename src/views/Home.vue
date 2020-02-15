dab786591e1425beb40314e139e310e355ac47ed
Saloni Agrawal <saloniagrawal250797@gmail.com>
2020-02-15 22:09:23 +0530

    Procfile added

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,5 @@
 <template>
-  <v-app: style="{background:$vuetify.theme.themes.light.background}">
+  <v-app style="{background:$vuetify.theme.themes.light.background}">
     <v-container>
       <v-layout row class="ma-4">
         <v-app style="background-color: grey lighten-1;" class="rounded"></v-app>
@@ -8,7 +8,7 @@
         </v-row>
       </v-layout>
     </v-container>
-  </v-app:>
+  </v-app>
 </template>
 
 <script>
